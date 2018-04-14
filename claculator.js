3b0f8bb042549051cbfdfbdf25946435be19bce1
Amr A. Abdelmoez <amradel88@live.com>
2018-04-14 12:33:39 +0200

    feat: bind NumPad keys to UI calculator buttons

---
+++ claculator.js
@@ -0,0 +1,45 @@
+document.getElementById('calculator').addEventListener('click', function (btn) {
+    if (btn.target.className === "btn") {
+        console.log(btn.target.innerHTML);
+    }
+})
+
+document.addEventListener("keydown", function (key) {
+    switch (key.keyCode) {
+        case 96: document.getElementById('no0').click();
+            break;
+        case 97: document.getElementById('no1').click();
+            break;
+        case 98: document.getElementById('no2').click();
+            break;
+        case 99: document.getElementById('no3').click();
+            break;
+        case 100: document.getElementById('no4').click();
+            break;
+        case 101: document.getElementById('no5').click();
+            break;
+        case 102: document.getElementById('no6').click();
+            break;
+        case 103: document.getElementById('no7').click();
+            break;
+        case 104: document.getElementById('no8').click();
+            break;
+        case 105: document.getElementById('no9').click();
+            break;
+// =====================================================
+        case 8: document.getElementById('del').click();
+            break;
+        case 111: document.getElementById('divide').click();
+            break;
+        case 106: document.getElementById('multiply').click();
+            break;
+        case 109: document.getElementById('subtract').click();
+            break;
+        case 107: document.getElementById('add').click();
+            break;
+        case 110: document.getElementById('decimal').click();
+            break;
+        case 13: document.getElementById('equal').click();
+            break;
+    }
+})
